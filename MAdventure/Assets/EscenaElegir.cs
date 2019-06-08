f79ee83d38cafd3b5669cb08b2b71c428983a05c
daviro05 <daviro05@ucm.es>
2019-06-08 13:37:48 +0200

    Actualización de Juegos Unity

---
+++ MAdventure/Assets/EscenaElegir.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscenaElegir : MonoBehaviour
+{
+	public void cambiarescena(string nombredeescena)
+	{
+
+	
+	}
+}
